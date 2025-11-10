bcc35429bee91c91c468a5433f6cd1da0bf33439
emilsvagelis <emilvage@madona.edu.lv& C:/Users/skolens/AppData/Local/Programs/Python/Python310/python.exe c:/Users/skolens/Desktop/ProgrammesanaEmilsV/25nov/macas_2024/darbs.py>
2025-11-10 12:54:18 +0200

    atrisinu 3. uzdevumu

--- 3uzdevums.py
+++ 3uzdevums.py
@@ -4,3 +4,11 @@ pirmās un pēdējās krāsas no šī saraksta.
 
 krasa_saraksts = ["sarkans", "zaļš", "balts", "melns"]
 """
+
+krasa_saraksts = ["sarkans", "zaļš", "balts", "melns"] # saraksts/masīvs
+pirma_krasa=krasa_saraksts[0]
+print(f"Pirmā krāsa sarakstā ir {pirma_krasa}")
+
+# pedejo krasu
+pedeja_krasa=krasa_saraksts[-1]
+print(f"Pedejais elements ir {pedeja_krasa}")
